4a7238b392d820b77328290a35a07f6c1259d5ae
Javirub <javi.rubio.roca@gmail.com>
2024-10-03 17:51:49 +0200

    Fix response when non existent ID is passed as parameter to getProductById

--- src/main/java/com/laberit/sina/bootcamp/modulo3/spring_web/controller/frontoffice/ProductFrontRestController.java
+++ src/main/java/com/laberit/sina/bootcamp/modulo3/spring_web/controller/frontoffice/ProductFrontRestController.java
@@ -2,7 +2,10 @@ package com.laberit.sina.bootcamp.modulo3.spring_web.controller.frontoffice;
 
 import com.laberit.sina.bootcamp.modulo3.spring_web.model.Product;
 import com.laberit.sina.bootcamp.modulo3.spring_web.service.ProductServiceImpl;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,10 +13,16 @@ import org.springframework.web.bind.annotation.*;
 public class ProductFrontRestController {
     @Autowired
     private ProductServiceImpl productServiceImpl;
+
     // Obtener un producto por id [READ]
     @GetMapping("/{id}")
     @ResponseBody
-    public Product getProductById(@PathVariable Long id) {
-        return productServiceImpl.getProductById(id);
+    public ResponseEntity<?> getProductById(@PathVariable Long id) {
+        try {
+            Product product = productServiceImpl.getProductById(id);
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity<>("No se ha encontrado el producto con id: "+ id,HttpStatus.NOT_FOUND);
+        }
     }
 }
