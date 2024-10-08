0f2202a82cac58313cef5e7307167e7861c802eb
Javier Rubio Roca <jrubio@laberit.com>
2024-10-08 13:55:47 +0200

    Fixed success.html view when make order properly

--- src/main/java/com/laberit/sina/bootcamp/modulo3/spring_web/controller/frontoffice/OrderFrontController.java
+++ src/main/java/com/laberit/sina/bootcamp/modulo3/spring_web/controller/frontoffice/OrderFrontController.java
@@ -1,11 +1,13 @@
 package com.laberit.sina.bootcamp.modulo3.spring_web.controller.frontoffice;
 
+import com.laberit.sina.bootcamp.modulo3.spring_web.dto.ProductDTO;
 import com.laberit.sina.bootcamp.modulo3.spring_web.model.Product;
 import com.laberit.sina.bootcamp.modulo3.spring_web.model.User;
 import com.laberit.sina.bootcamp.modulo3.spring_web.repository.ProductRepository;
 import com.laberit.sina.bootcamp.modulo3.spring_web.repository.UserRepository;
 import com.laberit.sina.bootcamp.modulo3.spring_web.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,7 +34,9 @@ public class OrderFrontController {
             User user = userRepository.findByEmailOrThrow(principal.getName());
             Product product = productRepository.findById(productId).orElseThrow();
             orderService.createOrder(product, user, quantity);
-            model.addAttribute("product", product);
+            String lang = LocaleContextHolder.getLocale().getLanguage();
+            ProductDTO productDTO = new ProductDTO(product, lang);
+            model.addAttribute("productName", productDTO.getName());
             model.addAttribute("quantity", quantity);
             return "order/success";
         } catch (Exception e) {
